d866348ed2a42fbeb42663356d955e314b3c0ac9
Alexandr Galanov <alexandr.galanov@gmail.com>
2021-11-19 13:54:22 +0300

    Косметические изменения

--- postgres_to_es/utils.py
+++ postgres_to_es/utils.py
@@ -1,8 +1,7 @@
 import logging
 import time
-from functools import wraps
-
 from dotenv import find_dotenv, load_dotenv
+from functools import wraps
 
 
 def load_env():
@@ -11,6 +10,7 @@ def load_env():
 
 def get_logger(name: str) -> logging.Logger:
     return logging.getLogger(name)
+
 
 def generate_state_name(table_name: str) -> str:
     """
@@ -21,8 +21,9 @@ def generate_state_name(table_name: str) -> str:
     """
     return '_'.join([table_name, 'updated_at'])
 
+
 def backoff(
-    start_sleep_time: float = 0.1, factor: int = 2, border_sleep_time: int = 10
+        start_sleep_time: float = 0.1, factor: int = 2, border_sleep_time: int = 10
 ):
     def func_wrapper(func):
         @wraps(func)
